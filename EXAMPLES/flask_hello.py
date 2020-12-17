709dc6dbb49d80a49280e361369259766abbb7b6
jstrickler <jstrickler@gmail.com>
2020-12-17 09:29:45 -0500

    end chapter 9

--- EXAMPLES/flask_hello.py
+++ EXAMPLES/flask_hello.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 from flask import Flask
+# add extensions here...
 
-app = Flask(__name__)  # <1>
+app = Flask(__name__)  # <1>  add config info here
 
+#  www.cia.gov/country/Spain
 
-# @time_stamp
-# @ham
-@app.route('/')  # <2>
-def index():  # <3>
-    return '<h1>Hello, Flask world!</h1>' # <4>
+@app.route('/country/<string:country>')  # <2>
+def visit(country):  # <3>
+    return f'<h1>Visit beautiful {country}!</h1>' # <4>
+
+@app.route('/')
+def index():
+    return '<h1>Hello from this silly app!</h1>'
+
+@app.route('/animal/<string:animal>')
+def zoo(animal):
+    return(f"<h1>{animal}s are wonderful</h1>")
 
 
 # app.register_route(index, '/')
